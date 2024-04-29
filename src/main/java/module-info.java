b60f8b54b2f3d911ca7eeba721dcaa216d1de737
BoseV <46490582+BoseVJeff@users.noreply.github.com>
2024-04-29 11:53:39 +0530

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module io.github.bosev.flight_booking {
+	requires javafx.controls;
+	requires javafx.fxml;
+
+	requires org.controlsfx.controls;
+	requires com.dlsc.formsfx;
+	requires net.synedra.validatorfx;
+	requires org.kordamp.ikonli.javafx;
+	requires org.kordamp.bootstrapfx.core;
+
+	opens io.github.bosev.flight_booking to javafx.fxml;
+	exports io.github.bosev.flight_booking;
+}
